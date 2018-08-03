bad41f0356ea3682a2d64e337e7521e220f8ab03
BarryCC <643987053@qq.com>
2018-08-03 17:29:31 +0800

    change

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 import './App.css'
-import logo from '../assets/logo.svg'
-import Main from './main/Main'
+import logo from './assets/images/logo.svg'
+import Container from './pages/Container'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*** 头部 ***/}
+        {/*** 左侧菜单 ***/}
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <ul>
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/roster/10'>Roster</Link></li>
+              <li><Link to='/example1'>example1</Link></li>
           </ul>
         </div>
-        {/*** 主体内容 ***/}
-        <Main />
+        {/*** 右侧内容组件 ***/}
+        <Container />
       </div>
     );
   }
---
+++ src/pages/Container.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import './container.css'
+
+import Clock from '../component/clock/clock'
+
+import Home from './home/Home'
+import Roster from './roster/Roster'
+import Example1 from './example1/Example1'
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Clock />
+
+        <Switch>
+          {/*** 默认也 ***/}
+          <Route exact path='/' component={Home}/>
+          {/*** 传参实例 ***/}
+          <Route path='/roster/:id' component={Roster}/>
+          {/*** 实例1 ***/}
+          <Route path='/example1' component={Example1}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Main;
