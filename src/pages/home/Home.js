b625271e3247304ab2265824e3585fb55e2c5110
BarryCC <643987053@qq.com>
2018-08-29 15:40:19 +0800

    add some example

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,93 @@
 import React, { Component } from 'react';
 
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div className="home">
-        this is react + router test.
+        {/* (e) => this.handleClick(e) 此种情况就不需要bind this */}
+        <button onClick={this.handleClick}>
+          Click me
+        </button>
+
+        <LoginControl />
       </div>
     );
   }
---
+++ src/template.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+class components extends Component {
+  render() {
+    return (
+      <div className="components"></div>
+    );
+  }
+}
+
+export default components;
